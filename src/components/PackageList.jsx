816b842878bda14da3622c99640f389dc91a6337
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-27 18:26:01 +0100

    separate package details components

--- src/components/PackageList.jsx
+++ src/components/PackageList.jsx
@@ -1,23 +1,20 @@
+import useFetch from "../services/useFetch";
+import Spinner from "./shared/Spinner";
 import {
   Accordion,
   AccordionItem,
   AccordionItemHeading,
   AccordionItemButton,
-  AccordionItemPanel,
 } from "react-accessible-accordion";
-import { Link } from "react-router-dom";
-import useFetch from "../services/useFetch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
-import Spinner from "./shared/Spinner";
-import moment from "moment";
+import PackageDetails from "./PackageDetails";
 
 export default function PackageList() {
   const { data, loading, error } = useFetch(
     "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840"
   );
-
-  if (error) return console.log("error");
+  if (error) throw error;
   if (loading || !data) return <Spinner />;
 
   const packages = data.map((item) => (
@@ -36,44 +33,7 @@ export default function PackageList() {
           </h2>
         </AccordionItemButton>
       </AccordionItemHeading>
-      <AccordionItemPanel>
-        <div className="info-content">
-          <h3>
-            <FontAwesomeIcon icon={solid("signal")} />
-            Status:
-          </h3>
-          <label className={item.status === "delivered" ? "greenColor" : ""}>
-            {item.status}
-          </label>
-        </div>
-        <div className="info-content">
-          <h3>
-            <FontAwesomeIcon icon={solid("calendar")} />
-            Estimated Time of Delivery:
-          </h3>
-          <label>{moment(item.eta).format("llll")}</label>
-        </div>
-        <div className="info-content">
-          <h3>
-            <FontAwesomeIcon icon={solid("calendar")} />
-            Last Updated:
-          </h3>
-          <label>{moment(item.last_updated).format("llll")}</label>
-        </div>
-        {item.notes !== null && (
-          <div className="info-content">
-            <h3>
-              <FontAwesomeIcon icon={solid("message")} />
-              Notes:
-            </h3>
-            <label>{item.notes}.</label>
-          </div>
-        )}
-        <Link to={`/listing/${item.id}`} className="track-link">
-          Track Package
-          <FontAwesomeIcon icon={solid("chevron-right")} />
-        </Link>
-      </AccordionItemPanel>
+      <PackageDetails currentProduct={item} />
     </AccordionItem>
   ));
 
