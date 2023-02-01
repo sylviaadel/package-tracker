c6064455fc59d384652968c89e339acebd33af0e
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-01 22:57:18 +0100

    add not found page and enhance map styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/shared/Navbar";
 import Welcome from "./pages/Welcome";
 import Packages from "./pages/Packages";
 import Location from "./pages/Location";
+import NotFound from "./pages/NotFound";
 import "./styles/style.scss";
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/packages" element={<Packages />} />
         <Route path="/packages/:id" element={<Location />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
