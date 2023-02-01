de347461508a1132f66fcf7467cd4833fbde1c6a
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-01 20:24:56 +0100

    Refactor code

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import Map from "../components/Map";
+import useFetch from "../services/useFetch";
 
 export default function Location() {
   return (
