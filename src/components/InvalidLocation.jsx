23be96929caf5f22ef6e9b46278b679a5872284a
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-02 12:12:40 +0100

    Add error for wrong ID

--- src/components/InvalidLocation.jsx
+++ src/components/InvalidLocation.jsx
@@ -1,15 +1,12 @@
-import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function NotFound() {
-  let location = useLocation();
-
   return (
     <div className="error">
       <h1>
         <FontAwesomeIcon icon={solid("triangle-exclamation")} />
-        Resourse not found at {location.pathname}
+        Invalid Location ID
       </h1>
     </div>
   );
