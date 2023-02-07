30379cdf71c12db46cb8560e1dae10264506dd41
Eduardo <el_alienx@hotmail.com>
2023-02-07 17:46:38 +0100

    partial refactor

--- src/components/PackageDetails.jsx
+++ src/components/PackageDetails.jsx
@@ -4,47 +4,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { customizeText } from "../helpers/PackageDetails";
 
+labels: {
+  delivery: {
+    title: "Estimate Time of Delivery"
+    icon: "wifi"
+  },
+  status: {
+    title: "Status",
+    icon: "wifi",
+  }
+}
+
 // You where doing so go until this part...
 export default function PackageDetails(props) {
+  // Properties
+  const deliverCSS = currentProduct.status === "delivered" ? "greenColor" : "";
+
   return (
     <>
-      <div className="info-content">
+      <header className="info-content">
         <h3>
           <FontAwesomeIcon icon={solid("signal")} />
           Status
         </h3>
-        <label
-          className={
-            props.currentProduct.status === "delivered" ? "greenColor" : ""
-          }
-        >
+        <label className={deliverCSS}>
           {customizeText(props.currentProduct.status)}
         </label>
-      </div>
-      <div className="info-content">
-        <h3>
-          <FontAwesomeIcon icon={solid("calendar")} />
-          Estimated Delivery Time
-        </h3>
-        <label>{moment(props.currentProduct.eta).format("llll")}</label>
-      </div>
-      <div className="info-content">
-        <h3>
-          <FontAwesomeIcon icon={solid("calendar")} />
-          Last Updated
-        </h3>
-        <label>
-          {moment(props.currentProduct.last_updated).format("llll")}
-        </label>
-      </div>
-      {props.currentProduct.notes !== null && (
-        <div className="info-content">
-          <h3>
-            <FontAwesomeIcon icon={solid("message")} />
-            Notes
-          </h3>
-          <label>{props.currentProduct.notes}.</label>
-        </div>
+      </header>
+      <InfoContent title="Status" icon="wifi" text="Delivered" />
+      <InfoContent title="Estimated Time of De" icon="wifi" text="Delivered" />
+      {currentProduct.notes !== null && (
+        <InfoContent title={labels.delivery.title} icon={lav} text="Delivered"/>
       )}
       <Link to={`/packages/${props.currentProduct.id}`} className="track-link">
         Track Package
